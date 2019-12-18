054ed77e64ecaf1612f2bf303e54da726c0b4ca2
vinicius-colutti <viniciuscolutti@hotmail.com>
2019-12-18 11:54:09 -0300

    Adding perfil

--- src/main/java/com/viniciuscolutti/moonrain/services/DBService.java
+++ src/main/java/com/viniciuscolutti/moonrain/services/DBService.java
@@ -20,6 +20,7 @@ import com.viniciuscolutti.moonrain.domain.Pedido;
 import com.viniciuscolutti.moonrain.domain.Produto;
 import com.viniciuscolutti.moonrain.domain.itemPedido;
 import com.viniciuscolutti.moonrain.domain.enums.EstadoPagamento;
+import com.viniciuscolutti.moonrain.domain.enums.Perfil;
 import com.viniciuscolutti.moonrain.domain.enums.TipoCliente;
 import com.viniciuscolutti.moonrain.repositories.CategoriaRepository;
 import com.viniciuscolutti.moonrain.repositories.CidadeRepository;
@@ -126,13 +127,18 @@ public class DBService {
 		Cliente cli1 = new Cliente(null, "Maria Silva", "viniciuscolutti@hotmail.com", "50382435842", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("11953534927", "1141586682"));
 		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "viniciuscolutti1203@gmail.com", "75317600022", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.addPerfil(Perfil.ADMIN);
+		cli2.getTelefones().addAll(Arrays.asList("6927288185", "69996152487"));
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
-		
+		Endereco e3 = new Endereco(null, "Rua Rio Negro", "563", "Sala 05", "Jardim dos Migrantes", "76900647", cli2, c2);
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
