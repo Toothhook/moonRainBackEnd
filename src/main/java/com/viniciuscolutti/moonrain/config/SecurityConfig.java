0fc89e7644e8535bec585801741ccb6ec0e81047
vinicius-colutti <viniciuscolutti@hotmail.com>
2019-12-19 08:52:50 -0300

    Authorization token JWT

--- src/main/java/com/viniciuscolutti/moonrain/config/SecurityConfig.java
+++ src/main/java/com/viniciuscolutti/moonrain/config/SecurityConfig.java
@@ -19,13 +19,24 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.viniciuscolutti.moonrain.security.JWTAuthenticationFilter;
+import com.viniciuscolutti.moonrain.security.JWTAuthorizationFilter;
+import com.viniciuscolutti.moonrain.security.JWTUtil;
+
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 	
 	private static final String[] PUBLIC_MATCHERS = {
 			"/h2-console/**"
@@ -33,7 +44,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
-			"/categorias/**",
+			"/categorias/**"
+	};
+	
+	private static final String[] PUBLIC_MATCHERS_POST = {
 			"/clientes/**"
 	};
 	
@@ -48,9 +62,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+		.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 	
 	@Bean
--- src/main/java/com/viniciuscolutti/moonrain/services/ClienteService.java
+++ src/main/java/com/viniciuscolutti/moonrain/services/ClienteService.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.viniciuscolutti.moonrain.domain.Cidade;
 import com.viniciuscolutti.moonrain.domain.Cliente;
 import com.viniciuscolutti.moonrain.domain.Endereco;
+import com.viniciuscolutti.moonrain.domain.enums.Perfil;
 import com.viniciuscolutti.moonrain.domain.enums.TipoCliente;
 import com.viniciuscolutti.moonrain.dto.ClienteDTO;
 import com.viniciuscolutti.moonrain.dto.ClienteNewDTO;
 import com.viniciuscolutti.moonrain.repositories.ClienteRepository;
 import com.viniciuscolutti.moonrain.repositories.EnderecoRepository;
+import com.viniciuscolutti.moonrain.security.UserSS;
+import com.viniciuscolutti.moonrain.services.expections.AuthorizationException;
 import com.viniciuscolutti.moonrain.services.expections.DataIntegrityException;
 import com.viniciuscolutti.moonrain.services.expections.ObjectNotFoundException;
 
@@ -36,6 +39,12 @@ public class ClienteService {
 	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
+		
+		UserSS user = UserService.authenticated();
+		if(user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
